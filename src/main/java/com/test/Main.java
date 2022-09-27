fbb9f3387281781a2793d22c6269ebda3347b554
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2022-09-27 23:43:29 +0300

    Создание функционала. Далле привдем код в наглядный вид.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,7 +1,100 @@
 package com.test;
 
+import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+
 public class Main {
+
+    private static ArrayList<String> nArray = new ArrayList<>();
+    private static ArrayList<String> mArray = new ArrayList<>();
+    private static ArrayList<String> result = new ArrayList<>();
+
     public static void main(String[] args) {
 
+        getTwoArrayFromString(TestString.INPUT);
+
+        printArray(nArray);
+        printArray(mArray);
+        System.out.println("RESULT: -> ");
+        deleteEmptyElements(nArray);
+        deleteEmptyElements(mArray);
+        compareArray(nArray, mArray);
+        printArray(result);
+
+
     }
+
+
+    public static boolean isDigitString(String string){
+        int value;
+        if (string == null || string.equals("")){
+            return false;
+        }
+
+        try {
+            value = Integer.parseInt(string);
+            return true;
+        }catch (NumberFormatException e){
+
+        }
+        return false;
+    }
+
+    public static void getTwoArrayFromString(String string){
+        String[] strings = string.split("\n");
+
+        int count = 0;
+
+        for (String s:strings) {
+            if (isDigitString(s)){
+                count++;
+            }
+            if (count == 1){
+                nArray.add(s);
+            }
+            if (count == 2){
+                mArray.add(s);
+            }
+        }
+    }
+
+    public static void printArray(ArrayList<String> arrayList){
+
+        for (String s:arrayList) {
+            System.out.println(s);
+        }
+    }
+
+
+    public static void deleteEmptyElements(ArrayList<String> arrayList){
+        arrayList.remove(0);
+        for(int i = 0; i < arrayList.size(); i++){
+            if (arrayList.get(i).equals("")){
+                arrayList.remove(i);
+            }
+        }
+    }
+
+    public static void compareArray(ArrayList<String> first, ArrayList<String> second){
+        for (String s: first) {
+            String temp = "\n";
+            temp += s+":";
+            for (String ss: second) {
+                if (compareStrings(s, ss)){
+                    temp = temp+ss;
+                }
+            }
+            result.add(temp);
+
+        }
+    }
+
+    public static boolean compareStrings(String first, String second){
+        if (StringUtils.contains(first, second)){
+            return true;
+        }
+        return false;
+    }
+
+
 }
---
+++ src/main/java/com/test/TestString.java
@@ -0,0 +1,33 @@
+package com.test;
+
+public class TestString {
+    public static final String INPUT = "" +
+            "4\n" +
+            "\n" +
+            "гвоздь\n" +
+            "\n" +
+            "шуруп\n" +
+            "\n" +
+            "краска синяя\n" +
+            "\n" +
+            "ведро для воды\n" +
+            "\n" +
+            "3\n" +
+            "\n" +
+            "краска\n" +
+            "\n" +
+            "корыто для воды\n" +
+            "\n" +
+            "шуруп 3х1.5";
+
+    public static final String TEST_STRING2 = ""+
+            "1\n" +
+            "\n" +
+            "Бетон с присадкой\n" +
+            "\n" +
+            "2\n" +
+            "\n" +
+            "присадка для бетона\n" +
+            "\n" +
+            "доставка";
+}
